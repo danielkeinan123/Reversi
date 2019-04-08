b5b333fb553818cf21486739ba2a5c53ba68e76f
chenflor <49087420+chenflor@users.noreply.github.com>
2019-04-08 20:21:56 +0300

    Add files via upload

--- reversiGame.js
+++ reversiGame.js
@@ -1,10 +1,39 @@
 
 
-var gameTable = document.querySelector(".gameTable");
-var newGame = document.querySelector("#new");
-var tableSize = 10;
-var blackTurn = true;
-var cells = [];
+let gameTable = document.querySelector(".gameTable");
+let newGame = document.querySelector("#new");
+let forfit = document.querySelector("#forfit");
+
+let gameStarted = false;
+
+const TABLE_SIZE = 10;
+let blackTurn = true;
+let cells = [];
+let d = new Date();
+
+let statistics ={
+	howManyTurns : 0,
+	startTime    : d.getTime(),
+	timePassed   : 0,
+	avgTimeForPlayerMove : 0,
+	numberOfTimesBlackReachedTwoCircles : 0,
+	numberOfTimesWhiteReachedTwoCircles : 0,
+	whiteScore : 2,
+	blackScore : 2
+};
+
+const whoWon = {
+	NoOne : 0,
+	White : 1,
+	Black : 2
+};
+
+const movesOnTheTable = {
+    addToColAndRow:		1,
+    subToColAndRow:		2,
+    addToColSubToRow:	3,
+    subToColAddToRow:	4
+};
 
 function min (a,b){
 	if(a>b){
@@ -23,10 +52,10 @@ function max(a,b){
 	}
 }
 
-for (var i = 0; i < tableSize; i++) {
+for (var i = 0; i < TABLE_SIZE; i++) {
 		var row = gameTable.insertRow(i);
 		cells[i]= [];
-		for (var j = 0; j < tableSize; j++) {
+		for (var j = 0; j < TABLE_SIZE; j++) {
 			var col = row.insertCell(j);
 			col.classList.add("cell");
 			col.addEventListener("click", function(){
@@ -40,12 +69,12 @@ for (var i = 0; i < tableSize; i++) {
 setValidCells();
 function initCellDiv(cell,i,j){
 	var circleDiv = document.createElement("DIV");
-	if((i == 4 & j == 4) | (i == 5 & j == 5)){
+	if((i === 4 & j === 4) | (i === 5 & j === 5)){
 		circleDiv.classList.add("circle");
 		cell.appendChild(circleDiv);
 		circleDiv.classList.add("blackCircle");
 	}
-	else if((i == 5 & j == 4) | (i == 4 & j == 5)){
+	else if((i === 5 & j === 4) | (i === 4 & j === 5)){
 		circleDiv.classList.add("circle");
 		cell.appendChild(circleDiv);
 		circleDiv.classList.add("whiteCircle");
@@ -54,39 +83,39 @@ function initCellDiv(cell,i,j){
 
 function nearAFullCell(i,j){
 	var ans1 = false;
-	if(i == 0 && j == 0){
+	if(i === 0 && j === 0){
 		ans1 = cells[i][j+1].hasChildNodes() ||
 		cells[i+1][j].hasChildNodes() ||  cells[i+1][j+1].hasChildNodes();
 	}
-	else if(i == 0 && j == 9){
+	else if(i === 0 && j === 9){
 		ans1 =  cells[i][j-1].hasChildNodes() || cells[i+1][j-1].hasChildNodes() ||
 		cells[i+1][j].hasChildNodes();
 	}
-	else if(i == 9 && j == 9){
+	else if(i === 9 && j === 9){
 		ans1 = cells[i-1][j-1].hasChildNodes() ||  cells[i-1][j].hasChildNodes() ||
 			cells[i][j-1].hasChildNodes();
 	}
-	else if(i == 9 && j == 0){
+	else if(i === 9 && j === 0){
 		ans1 = cells[i-1][j].hasChildNodes() ||
 		cells[i-1][j+1].hasChildNodes() || cells[i][j+1].hasChildNodes();
 	}
-	else if(i == 0){
+	else if(i === 0){
 		ans1 = cells[i][j-1].hasChildNodes() ||
 			 cells[i][j+1].hasChildNodes() ||  cells[i+1][j-1].hasChildNodes() ||
 			 cells[i+1][j].hasChildNodes() ||  cells[i+1][j-1].hasChildNodes();
 	}
-	else if(i == 9){
+	else if(i === 9){
 		ans1 = cells[i-1][j-1].hasChildNodes() ||  cells[i-1][j].hasChildNodes() ||
 		cells[i-1][j+1].hasChildNodes() ||  cells[i][j-1].hasChildNodes() ||
 		cells[i][j+1].hasChildNodes();
 	}
-	else if (j == 0){
+	else if (j === 0){
 		ans1 = cells[i-1][j].hasChildNodes() ||
 			 cells[i-1][j+1].hasChildNodes() ||
-			 cells[i][j+1].hasChildNodes() || cells[i+1][j+1].hasChildNodes()
+			 cells[i][j+1].hasChildNodes() || cells[i+1][j+1].hasChildNodes();
 			 cells[i+1][j].hasChildNodes();
 	}
-	else if(j == 9){
+	else if(j === 9){
 		ans1 = cells[i-1][j-1].hasChildNodes() ||  cells[i-1][j].hasChildNodes() ||
 			cells[i][j-1].hasChildNodes() ||
 			cells[i+1][j-1].hasChildNodes() ||
@@ -103,10 +132,10 @@ function nearAFullCell(i,j){
 }
 
 function setValidCells(){
-	for(var i = 0; i < tableSize; i++) {
-		for(var j = 0; j < tableSize; j++){
+	for(var i = 0; i < TABLE_SIZE; i++) {
+		for(var j = 0; j < TABLE_SIZE; j++){
 			if(!(cells[i][j].hasChildNodes())){
-				if(nearAFullCell(i,j) == true){
+				if(nearAFullCell(i,j) === true){
 					cells[i][j].classList.add("validCell");
 					cells[i][j].classList.remove("invalidCell");
 				}
@@ -131,21 +160,25 @@ function setCellsToNewColor(cell, newColor, oldColor){
 	function smaller(i,to){
 		return i < to;
 	}
-	findCellsToChangeInRow(bigger,tableSize-1, colIndex ,false,rowIndex,newColor,oldColor);
+	findCellsToChangeInRow(bigger,TABLE_SIZE-1, colIndex ,false,rowIndex,newColor,oldColor);
 	findCellsToChangeInRow(smaller,0, colIndex , true,rowIndex,newColor,oldColor);	
-	findCellsToChangeInColum(bigger, tableSize-1, rowIndex, false, colIndex, newColor, oldColor);
+	findCellsToChangeInColum(bigger, TABLE_SIZE-1, rowIndex, false, colIndex, newColor, oldColor);
 	findCellsToChangeInColum(smaller, 0, rowIndex, true, colIndex,newColor, oldColor);
 	//down vertical
 	i = max(0,rowIndex-colIndex);
 	j = max(0,colIndex-rowIndex);
 	to = colIndex;
-	findCellsToChangeInVertical(smaller, j, to, true, i, newColor, oldColor);
-	i = max(tableSize-1,(tableSize-1)-rowIndex);
-	j = min(tableSize-1,(tableSize-1)-colIndex);
-	findCellsToChangeInVertical(bigger, j, to, false, i, newColor, oldColor);
+	findCellsToChangeInVertical(smaller, j, to, movesOnTheTable.addToColAndRow, i, newColor, oldColor);
+	i = min(TABLE_SIZE-1,(rowIndex+((TABLE_SIZE-1)-colIndex)));
+	j = min(TABLE_SIZE-1,(colIndex+((TABLE_SIZE-1)-rowIndex)));
+	findCellsToChangeInVertical(bigger, j, to, movesOnTheTable.subToColAndRow, i, newColor, oldColor);
 	//up verticl
-	//findCellsToChangeInVertical(comper, j, to, add, i, newColor, oldColor);
-	//findCellsToChangeInVertical(comper, j, to, add, i, newColor, oldColor);
+	i = max(0,(rowIndex-((TABLE_SIZE-1)-colIndex)));
+	j = min(TABLE_SIZE-1,colIndex+rowIndex);
+	findCellsToChangeInVertical(bigger, j, to, movesOnTheTable.subToColAddToRow, i, newColor, oldColor);
+	i = min(TABLE_SIZE-1,colIndex+rowIndex);
+	j = max(0,(colIndex-((TABLE_SIZE-1)-rowIndex)));
+	findCellsToChangeInVertical(smaller, j, to, movesOnTheTable.addToColSubToRow, i, newColor, oldColor);
 
 }
 
@@ -196,12 +229,12 @@ function findCellsToChangeInColum(comper, j,to , add ,colIndex,newColor,oldColor
 	}
 }
 
-function findCellsToChangeInVertical(comper, j,to , add ,i,newColor,oldColor){
+function findCellsToChangeInVertical(comper, j,to , nextMove ,i,newColor,oldColor){
 	var foundMatch = false;
 	var setToColor = false;
-	console.log("befor while "+j+" "+to);
+	//console.log("befor while "+j+" "+to);
 	while(comper(j,to)){
-		console.log("in while i="+i+" j="+j+" to="+to);
+		//console.log("in while i="+i+" j="+j+" to="+to);
 		if(cells[i][j].hasChildNodes()){
 			if (cells[i][j].childNodes[0].classList.contains(newColor) && !foundMatch){
 				setToColor = true;
@@ -212,23 +245,33 @@ function findCellsToChangeInVertical(comper, j,to , add ,i,newColor,oldColor){
 				cells[i][j].childNodes[0].classList.remove(oldColor)
 			}
 		}
-		if(add){
-			j = j+1;
-			i = i+1;
-		}
-		else{
-			j = j-1;
-			i = i-1;
+		switch (nextMove){
+			case 1:
+				j = j+1;
+				i = i+1;
+				break;
+			case 2:
+				j = j-1;
+				i = i-1;
+				break;
+			case 3:
+				j = j+1;
+				i = i-1;
+				break;
+			case 4:
+				j = j-1;
+				i = i+1;
+				break;
 		}
 	}
 }
 
 function clickCell(cell){
 	if(cell.classList.contains("validCell")){
+
 		var newDiv = document.createElement("DIV");
 		newDiv.classList.add("circle");
 		cell.appendChild(newDiv);
-		var colorClass = "";
 		if (blackTurn){
 			newDiv.classList.add("blackCircle");
 			newColorClass = "blackCircle";
@@ -242,14 +285,115 @@ function clickCell(cell){
 		cell.classList.remove("validCell");
 		setValidCells();
 		setCellsToNewColor(cell, newColorClass, oldColorClass);
-		blackTurn = !blackTurn;
+		updateStatistics();
+		let winner = isThereAWinner();
+		if(winner!== whoWon.NoOne){
+			endGame(winner);
+		}
+		changeTurn()
+
 	}
 }
 
+function changeTurn() {
+	let playerTurn = document.getElementById("playerTurn");
+	if (blackTurn){
+		playerTurn.classList.add("whitePlayerTurn");
+		playerTurn.classList.remove("blackPlayerTurn");
+		document.getElementById("playerTurnText").innerHTML = "white Player Turn";
+	}
+	else{
+		playerTurn.classList.add("blackPlayerTurn");
+		playerTurn.classList.remove("whitePlayerTurn");
+		document.getElementById("playerTurnText").innerHTML = "black Player Turn";
+	}
+
+	blackTurn = !blackTurn;
+}
+
+
+function updateScoresCount() {
+	var blackCounter =0, whiteCounter =0;
+	for(var i =0; i < TABLE_SIZE; i++) {
+		for (var j = 0; j < TABLE_SIZE; j++) {
+			if (cells[i][j].hasChildNodes()) {
+				if (cells[i][j].childNodes[0].classList.contains("blackCircle")) {
+
+					blackCounter++;
+				} else if (cells[i][j].childNodes[0].classList.contains("whiteCircle")) {
+					whiteCounter++;
+				}
+			}
+		}
+	}
+
+	statistics.whiteScore = whiteCounter;
+	statistics.blackScore = blackCounter;
+
+	document.getElementById("blackPlayerScore").innerHTML = "Black Score:" + statistics.blackScore;
+	document.getElementById("whitePlayerScore").innerHTML = "White Score:" + statistics.whiteScore;
+
+}
+function isThereAWinner() {
+	var isFull = (statistics.whiteScore +statistics.blackScore  ===  TABLE_SIZE*TABLE_SIZE);
+	var ans;
+
+	if (isFull){
+		if (statistics.whiteScore > statistics.blackScore){
+			ans=  whoWon.White;
+		}
+		else{
+			ans = whoWon.Black;
+		}
+	}
+	else{
+		if (statistics.whiteScore ===0){
+			ans= whoWon.Black;
+		}
+		else if (statistics.blackScore ===0){
+			ans =  whoWon.White;
+		}
+		else{
+			ans =  whoWon.NoOne;
+		}
+	}
+
+	return ans;
+
+}
+function endGame(winner) {
+	statistics.timePassed = d.getTime() - statistics.startTime;
+	statistics.avgTimeForPlayerMove = statistics.timePassed/statistics.howManyTurns;
+	if(winner === whoWon.Black)
+		window.alert("GAME HAS ENDED, Congratulations Black");
+	else
+		window.alert("GAME HAS ENDED, Congratulations White");
+}
+
+function updatePlayersTwoTimes(){
+	if (statistics.whiteScore === 2){
+		statistics.numberOfTimesWhiteReachedTwoCircles++;
+	}
+	if (statistics.blackScore === 2){
+		statistics.numberOfTimesBlackReachedTwoCircles++;
+	}
+}
+
+function updateStatistics(){
+	updateScoresCount();
+	statistics.howManyTurns ++;
+	updatePlayersTwoTimes();
+	document.getElementById("numOfTurns").innerHTML = statistics.howManyTurns;
+	document.getElementById("timePassed").innerHTML = statistics.timePassed;
+	document.getElementById("avgTimeForPlayer").innerHTML = statistics.avgTimeForPlayerMove;
+	document.getElementById("numOfBlackWithTwoCircles").innerHTML = statistics.numberOfTimesBlackReachedTwoCircles;
+	document.getElementById("numOfWhiteWithTwoCircles").innerHTML = statistics.numberOfTimesWhiteReachedTwoCircles;
+}
 function createNewGame(){
 	var cellChild;
-	for (var i = 0; i < tableSize ; i++) {
-		for(var j = 0; j < tableSize; j++){
+	statistics.howManyTurns = 0;
+	for (var i = 0; i < TABLE_SIZE ; i++) {
+		for(var j = 0; j < TABLE_SIZE; j++){
 			cells[i][j].classList.remove("validCell");
 			cells[i][j].classList.remove("invalidCell");
 			if(cells[i][j].hasChildNodes()){
@@ -260,9 +404,26 @@ function createNewGame(){
 		}
 	}
 	setValidCells();
+
+	gameStarted = true;
+}
+
+function surrender(){
+	if(gameStarted){
+		if(blackTurn){
+			endGame(whoWon.White);
+		}
+		else {
+			endGame(whoWon.Black)
+		}
+	}
 }
 
 newGame.addEventListener("click",function(){
 	createNewGame();
+});
+
+forfit.addEventListener("click",function () {
+	surrender();
 });
 
